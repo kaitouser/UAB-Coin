0736b8b80d431a46d9b5846d7d8fdd2dfdd0ac60
adriend1102 <114516137+adriend1102@users.noreply.github.com>
2023-05-26 02:03:48 +0200

    QR v0.1

--- UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
+++ UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
@@ -1,12 +1,40 @@
 package uab.uab_coin.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import uab.uab_coin.R
+import uab.uab_coin.databinding.ActivityGetCoinsBinding
+import uab.uab_coin.databinding.ActivityMainBinding
+
 
 class GetCoinsActivity : AppCompatActivity() {
+    companion object {
+        const val RESULT = "RESULT"
+    }
+    private lateinit var binding: ActivityGetCoinsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_coins)
+        binding = ActivityGetCoinsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnScan.setOnClickListener {
+            val intent = Intent(applicationContext, ScanActivity::class.java)
+            startActivity(intent)
+        }
+
+        val result = intent.getStringExtra(RESULT)
+
+        if (result != null) {
+            if (result.contains("https://") || result.contains("http://")) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result))
+                startActivity(intent)
+            } else {
+                binding.result.text = result.toString()
+            }
+        }
     }
 }
