ad92b5db91a8ff9ccf821f5b970f790cce5d36f9
adriend1102 <114516137+adriend1102@users.noreply.github.com>
2023-05-27 13:05:10 +0200

    QR v0.3

--- UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
+++ UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
@@ -3,6 +3,9 @@ package uab.uab_coin.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+//import com.google.zxing.integration.android.IntentIntegrator
 
 import uab.uab_coin.databinding.ActivityGetCoinsBinding
 import uab.uab_coin.databinding.ActivityMainBinding
@@ -14,21 +17,39 @@ class GetCoinsActivity : DrawerBaseActivity()
     companion object {
         const val RESULT = "RESULT"
     }
-
+    private fun initScanner() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Sigue aprendiendo en CursoKotlin.com")
+        integrator.setTorchEnabled(true)
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "El valor escaneado es: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
     private lateinit var binding: ActivityGetCoinsBinding
-    var activityGetCoinsBinding: ActivityGetCoinsBinding? = null
+    //var binding: ActivityGetCoinsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityGetCoinsBinding = ActivityGetCoinsBinding.inflate(layoutInflater)
-        setContentView(activityGetCoinsBinding!!.root)
+        binding = ActivityGetCoinsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        binding.btnScan.setOnClickListener {
-            val intent = Intent(applicationContext, ScanActivity::class.java)
-            startActivity(intent)
-        }
+        binding.btnScan.setOnClickListener { initScanner() }
 
+
+        /*
         val result = intent.getStringExtra(RESULT)
 
         if (result != null) {
@@ -39,5 +60,6 @@ class GetCoinsActivity : DrawerBaseActivity()
                 binding.result.text = result.toString()
             }
         }
+        */
     }
 }
