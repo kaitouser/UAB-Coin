d071675c6135bcf0c4f92d1563e8169b42e62bda
SrKoko <paubermudezvalle@gmail.com>
2023-05-30 23:31:16 +0200

    Get coins fully implemented

--- UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
+++ UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
@@ -2,12 +2,26 @@ package uab.uab_coin.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.zxing.integration.android.IntentIntegrator
+import uab.uab_coin.R
+import uab.uab_coin.adapter.OfferAdapter
 
 import uab.uab_coin.databinding.ActivityGetCoinsBinding
+import uab.uab_coin.models.OfferModel
+import uab.uab_coin.models.UserModel
 
 class GetCoinsActivity : DrawerBaseActivity()
 {
@@ -27,14 +41,32 @@ class GetCoinsActivity : DrawerBaseActivity()
         integrator.setTorchEnabled(true)
         integrator.setBeepEnabled(true)
         integrator.initiateScan()
+
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
             if (result.contents == null) {
                 Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
             } else {
-                Toast.makeText(this, "El valor escaneado es: " + result.contents, Toast.LENGTH_LONG).show()
+                val inputString = result.contents.toString()
+                val parts = inputString.split("/")
+                val getCode = parts[0]
+                val coins = parts[1]
+
+                checkRedeem(getCode.toString()) { result ->
+
+                    if (result == "No")
+                    {
+                        addCoins(getCode.toString(),coins.toString(), result)
+                        Toast.makeText(this, coins + " coins redeemed", Toast.LENGTH_LONG).show()
+                    }
+                    else {
+                        Toast.makeText(this, "Coins already claimed", Toast.LENGTH_LONG).show()
+                    }
+                }
+
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
@@ -47,9 +79,81 @@ class GetCoinsActivity : DrawerBaseActivity()
         binding = ActivityGetCoinsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        userId = intent.getStringExtra("id").toString()
 
         binding.btnScan.setOnClickListener { initScanner() }
 
 
     }
+
+    private fun checkRedeem(getCode : String, callback: (String) -> Unit)  {
+
+        var redeemed = "No"
+
+        dbRef = FirebaseDatabase.getInstance().getReference("UserGets").child(userId)
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (snap in snapshot.children) {
+                        var key = snap.getKey()
+                        if (key.toString().equals(getCode)) {
+                            redeemed = "Yes"
+                        }
+                    }
+                }
+                callback(redeemed)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun addCoins(getCode : String, coins : String, result : String) {
+        var r = result
+
+            dbRef =
+                FirebaseDatabase.getInstance().getReference("UserGets").child(userId).child(getCode)
+            dbRef.setValue(coins)
+                .addOnCompleteListener {
+                    //Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
+
+                }.addOnFailureListener { err ->
+                    //Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
+                }
+
+            dbRef = FirebaseDatabase.getInstance().getReference("Users").child(userId)
+
+            dbRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val user = snapshot.getValue(UserModel::class.java)
+                        if (user != null) {
+                            dbRef =
+                                FirebaseDatabase.getInstance().getReference("Users").child(userId)
+                                    .child("userCoins")
+                            if (r == "No") {
+                                r = "Yes"
+                                dbRef.setValue(user.userCoins?.toInt()?.plus(coins.toInt()))
+                                    .addOnCompleteListener {
+                                        //Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
+
+                                    }.addOnFailureListener { err ->
+                                        //Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
+                                    }
+                            }
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+    }
+
 }
