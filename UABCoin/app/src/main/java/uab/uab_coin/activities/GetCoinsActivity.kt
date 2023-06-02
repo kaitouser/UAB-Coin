24e64186d70c9d5b6e426e0ba46aac3e347f850c
adriend1102 <114516137+adriend1102@users.noreply.github.com>
2023-06-02 15:10:53 +0200

    comVision v2.0

--- UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
+++ UABCoin/app/src/main/java/uab/uab_coin/activities/GetCoinsActivity.kt
@@ -3,16 +3,26 @@ package uab.uab_coin.activities
 import okhttp3.*
 import java.io.IOException
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+
+
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.widget.Toast
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -20,32 +30,36 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.zxing.integration.android.IntentIntegrator
 import okhttp3.Call
+import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.Response
 import org.json.JSONObject
 import uab.uab_coin.R
 import uab.uab_coin.R.id.btnTrash
+import uab.uab_coin.adapter.OfferAdapter
+
 import uab.uab_coin.databinding.ActivityGetCoinsBinding
+import uab.uab_coin.models.OfferModel
 import uab.uab_coin.models.UserModel
 import java.io.ByteArrayOutputStream
-
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class GetCoinsActivity : DrawerBaseActivity()
 {
-    private lateinit var binding: ActivityGetCoinsBinding
+
     private lateinit var dbRef : DatabaseReference
+    private lateinit var auth : FirebaseAuth
+
     private lateinit var userId : String
 
     companion object {
         const val RESULT = "RESULT"
-        private const val PICK_IMAGE_REQUEST = 1
-        private const val CAMERA_REQUEST = 2
-        private const val REQUEST_PERMISSION = 3
-        private const val REQUEST_IMAGE_CAPTURE = 4
-        private const val FUNCTION_URL = "https://europe-west3-uab-coin.cloudfunctions.net/function-1"
     }
 
-    private fun initScanner()
-    {
+    private fun initScanner() {
         val integrator = IntentIntegrator(this)
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
         //integrator.setPrompt("Sigue aprendiendo en CursoKotlin.com")
@@ -54,8 +68,8 @@ class GetCoinsActivity : DrawerBaseActivity()
         integrator.initiateScan()
 
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
-    {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
             if (result.contents == null) {
@@ -77,32 +91,20 @@ class GetCoinsActivity : DrawerBaseActivity()
                 }
 
             }
-        }
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            val imageView = findViewById<ImageView>(R.id.imageView)
-            imageView.setImageBitmap(imageBitmap)
-
-            sendImageToCloudFunction(imageBitmap)
         }else {
+            //Toast.makeText(this, "Imagen no enviada", Toast.LENGTH_LONG).show()
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
-
-    private fun sendImageToCloudFunction(imageBitmap: Bitmap)
-    {
+    private fun sendImageToCloudFunction(imageFile: File) {
         val url = "https://europe-west3-uab-coin.cloudfunctions.net/function-1"
-
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
-        val byteArray = byteArrayOutputStream.toByteArray()
 
         val requestBody = MultipartBody.Builder()
             .setType(MultipartBody.FORM)
             .addFormDataPart(
                 "image",
                 "captured_image.jpg",
-                RequestBody.create("image/jpeg".toMediaTypeOrNull(), byteArray)
+                imageFile.asRequestBody("image/jpeg".toMediaTypeOrNull())
             )
             .build()
 
@@ -112,7 +114,6 @@ class GetCoinsActivity : DrawerBaseActivity()
             .build()
 
         val client = OkHttpClient()
-
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread {
@@ -125,55 +126,118 @@ class GetCoinsActivity : DrawerBaseActivity()
             }
 
             override fun onResponse(call: Call, response: Response) {
-                val responseData = response.body?.string()
+                if (response.isSuccessful) {
+                    val responseData = response.body?.string()
 
-                if (responseData != null) {
-                    val result = JSONObject(responseData)
-                    val isTrash = result.getBoolean("isTrash")
-                    val message = if (isTrash) "Es basura" else "No es basura"
+                    if (responseData != null) {
+                        Log.d("ResponseData", responseData) // Imprimir responseData por pantalla
+                        val result = JSONObject(responseData)
+                        val labelsArray = result.getJSONArray("labels")
+                        val labels = mutableListOf<String>()
+                        for (i in 0 until labelsArray.length()) {
+                            labels.add(labelsArray.getString(i))
+                        }
 
-                    runOnUiThread {
-                        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+                        runOnUiThread {
+                            val message = if (labels.contains("trash")) "Es basura" else "No es basura"
+                            Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+                            Log.d("Labels", labels.toString()) // Imprimir etiquetas en el log
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(
+                                applicationContext,
+                                "Error al obtener la respuesta de la función en la nube",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 } else {
                     runOnUiThread {
                         Toast.makeText(
                             applicationContext,
-                            "Error al obtener la respuesta de la función en la nube",
+                            "Error al llamar a la función en la nube",
                             Toast.LENGTH_SHORT
                         ).show()
                     }
                 }
             }
+
         })
+        // Log de las dimensiones de la imagen
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(imageFile.absolutePath, options)
+        val imageWidth = options.outWidth
+        val imageHeight = options.outHeight
+        Log.d("ImageDimensions", "Width: $imageWidth, Height: $imageHeight")
     }
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        // Afegir barra d'eines
-        super.onCreate(savedInstanceState)
-        binding = ActivityGetCoinsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        //
-        userId = intent.getStringExtra("id").toString()
-        val btnTrash= findViewById<Button>(btnTrash)
-
-        binding.btnTrash.setOnClickListener{
-            startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
-        }
-        binding.btnScan.setOnClickListener { initScanner() }
+    private lateinit var binding: ActivityGetCoinsBinding
 
 
-    }
-    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result: ActivityResult ->
-        if(result.resultCode == Activity.RESULT_OK){
+    private var isImageCaptureRequested = false
+
+    private lateinit var imageFile: File
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (isImageCaptureRequested && result.resultCode == Activity.RESULT_OK) {
             val intent = result.data
             val imageBitmap = intent?.extras?.get("data") as Bitmap
             val imageView = findViewById<ImageView>(R.id.imageView)
             imageView.setImageBitmap(imageBitmap)
+
+            // Guardar la imagen en un archivo temporal
+            imageFile = createImageFile()
+            saveBitmapToFile(imageBitmap, imageFile)
+
+            // Ahora que se ha capturado la imagen, envíala a la función de la nube
+            sendImageToCloudFunction(imageFile)
+
+            isImageCaptureRequested = false // Restablecer el indicador a false
         }
+    }
+
+
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Crear un nombre de archivo único basado en la fecha y hora actual
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        )
+    }
+
+    private fun saveBitmapToFile(bitmap: Bitmap, file: File) {
+        try {
+            val fos = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.flush()
+            fos.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGetCoinsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userId = intent.getStringExtra("id").toString()
+        val btnTrash= findViewById<Button>(btnTrash)
+        binding.btnTrash.setOnClickListener {
+            // Establecer el indicador en true antes de iniciar la captura de imagen
+            isImageCaptureRequested = true
+            val captureIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startForResult.launch(captureIntent)
+        }
+        binding.btnScan.setOnClickListener { initScanner() }
+
+
     }
     private fun checkRedeem(getCode : String, callback: (String) -> Unit)  {
 
@@ -204,43 +268,45 @@ class GetCoinsActivity : DrawerBaseActivity()
     private fun addCoins(getCode : String, coins : String, result : String) {
         var r = result
 
-            dbRef =
-                FirebaseDatabase.getInstance().getReference("UserGets").child(userId).child(getCode)
-            dbRef.setValue(coins)
-                .addOnCompleteListener {
-                    //Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
+        dbRef =
+            FirebaseDatabase.getInstance().getReference("UserGets").child(userId).child(getCode)
+        dbRef.setValue(coins)
+            .addOnCompleteListener {
+                //Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
 
-                }.addOnFailureListener { err ->
-                    //Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
-                }
+            }.addOnFailureListener { err ->
+                //Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
+            }
 
-            dbRef = FirebaseDatabase.getInstance().getReference("Users").child(userId)
+        dbRef = FirebaseDatabase.getInstance().getReference("Users").child(userId)
 
-            dbRef.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        val user = snapshot.getValue(UserModel::class.java)
-                        if (user != null) {
-                            dbRef =
-                                FirebaseDatabase.getInstance().getReference("Users").child(userId)
-                                    .child("userCoins")
-                            if (r == "No") {
-                                r = "Yes"
-                                dbRef.setValue(user.userCoins?.toInt()?.plus(coins.toInt()))
-                                    .addOnCompleteListener {
-                                        //Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val user = snapshot.getValue(UserModel::class.java)
+                    if (user != null) {
+                        dbRef =
+                            FirebaseDatabase.getInstance().getReference("Users").child(userId)
+                                .child("userCoins")
+                        if (r == "No") {
+                            r = "Yes"
+                            dbRef.setValue(user.userCoins?.toInt()?.plus(coins.toInt()))
+                                .addOnCompleteListener {
+                                    //Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
 
-                                    }.addOnFailureListener { err ->
-                                        //Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
-                                    }
-                            }
+                                }.addOnFailureListener { err ->
+                                    //Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
+                                }
                         }
                     }
                 }
+            }
 
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-            })
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
     }
+
 }
